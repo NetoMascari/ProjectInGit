c8cedd747c6657fd53289c02b5bc488e44377756
Neto Mascari <ranulfomascari@hotmail.com>
2021-02-07 11:06:07 -0300

    Acrescentado repeticao for

--- Project/Program.cs
+++ Project/Program.cs
@@ -6,11 +6,15 @@ namespace Project
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world!");
+            
             DateTime d1 = DateTime.Now;
             Console.WriteLine(d1);
 
             DateTime d2 = DateTime.Parse("2021-11-30");
             Console.WriteLine(d2);
+
+            Console.ReadLine();
         }
     }
 }
